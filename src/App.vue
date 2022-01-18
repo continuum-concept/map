c4e787f7b3348418dcfea34ef142747fec86c37e
Benjamin Schnitzler <benjamin.schnitzler+git@googlemail.com>
2022-01-18 17:24:46 +0100

    centering on Caura River and cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
-<!--template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
-</template-->
-
 <template>
-  <div id="mymap"></div>
+  <div class="map"></div>
 </template>
 
 <script>
-// import { L } from 'leaflet';
 import * as L from 'leaflet';
 import '../vendor/tile.stamen';
 
 export default {
   mounted() {
     const layer = new L.StamenTileLayer('terrain');
-    const map = new L.Map('mymap', {
-      /* Center: Munich, Germany */
-      center: new L.LatLng(48.1, 11.5),
-      /* Show most of western Europe */
-      zoom: 6,
+    const map = new L.Map(this.$el, {
+      /* Center: Caura River in Venezuela */
+      center: new L.LatLng(6.310521, -64.523735),
+      zoom: 9,
     });
     map.addLayer(layer);
   },
 };
 </script>
 
-<style>
-#mymap { height: 100%; }
+<style scoped>
+.map {
+  height: 100%;
+  width: 100%;
+}
 @import '../node_modules/leaflet/dist/leaflet.css'
 </style>
