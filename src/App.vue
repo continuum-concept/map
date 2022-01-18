22bd604bb5ab2a3e69fad783b4fc9bc292929851
Benjamin Schnitzler <benjamin.schnitzler+git@googlemail.com>
2022-01-18 17:05:35 +0100

    first working map version without anything on it

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
-<template>
+<!--template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+</template-->
+
+<template>
+  <div id="mymap"></div>
 </template>
 
+<script>
+// import { L } from 'leaflet';
+import * as L from 'leaflet';
+import '../vendor/tile.stamen';
+
+export default {
+  mounted() {
+    const layer = new L.StamenTileLayer('terrain');
+    const map = new L.Map('mymap', {
+      /* Center: Munich, Germany */
+      center: new L.LatLng(48.1, 11.5),
+      /* Show most of western Europe */
+      zoom: 6,
+    });
+    map.addLayer(layer);
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+#mymap { height: 100%; }
+@import '../node_modules/leaflet/dist/leaflet.css'
 </style>
